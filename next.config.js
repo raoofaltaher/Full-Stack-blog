88e563937a3e90ebef6b6cd7007819f6444a73d6
RAOOF ALTAHER <raoofaltaher.business@gmail.com>
2025-04-04 17:11:16 +0300

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,26 +1,36 @@
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+})
+
 /** @type {import('next').NextConfig} */
-module.exports = {
-    // othor next config here...
-    compiler: {
-      removeConsole: process.env.NODE_ENV === 'production' ? true : false,
-    },
-    webpack: config => {
-      config.plugins.push(new VeliteWebpackPlugin())
-      return config
-    }
+const nextConfig = {
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production' ? true : false,
+  },
+  webpack: config => {
+    config.plugins.push(new VeliteWebpackPlugin())
+    return config
   }
-  
-  class VeliteWebpackPlugin {
-    static started = false
-    apply(/** @type {import('webpack').Compiler} */ compiler) {
-      // executed three times in nextjs
-      // twice for the server (nodejs / edge runtime) and once for the client
-      compiler.hooks.beforeCompile.tapPromise('VeliteWebpackPlugin', async () => {
-        if (VeliteWebpackPlugin.started) return
-        VeliteWebpackPlugin.started = true
-        const dev = compiler.options.mode === 'development'
-        const { build } = await import('velite')
-        await build({ watch: dev, clean: !dev })
-      })
-    }
+}
+
+module.exports = withMDX(nextConfig)
+
+class VeliteWebpackPlugin {
+  static started = false
+  apply(/** @type {import('webpack').Compiler} */ compiler) {
+    // executed three times in nextjs
+    // twice for the server (nodejs / edge runtime) and once for the client
+    compiler.hooks.beforeCompile.tapPromise('VeliteWebpackPlugin', async () => {
+      if (VeliteWebpackPlugin.started) return
+      VeliteWebpackPlugin.started = true
+      const dev = compiler.options.mode === 'development'
+      const { build } = await import('velite')
+      await build({ watch: dev, clean: !dev })
+    })
   }
+}
