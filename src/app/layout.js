6acbadbaf0c2284ca03542204cb94522401b8369
RAOOF ALTAHER <raoofaltaher.business@gmail.com>
2025-04-04 17:45:39 +0300

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-import "./globals.css";
+import "./globals.css?v=1";
 import { cx } from "@/src/utils";
 import { Inter, Manrope } from "next/font/google";
 import Header from "@/src/components/Header";
